725132ce9e608b01acb5d82bddbf1177f9a80ae3
Jack Gough <goughy000@gmail.com>
2021-12-06 12:27:31 +0000

    Day 6

--- day6/src/main/java/com/github/goughy000/Day6.java
+++ day6/src/main/java/com/github/goughy000/Day6.java
@@ -1,5 +1,11 @@
 package com.github.goughy000;
 
+import static com.github.goughy000.Collections2.first;
+import static com.github.goughy000.Collections2.parseInts;
+import static java.lang.System.arraycopy;
+
+import java.util.stream.LongStream;
+
 public class Day6 extends Solution {
 
   public static void main(String[] args) {
@@ -7,12 +13,28 @@ public class Day6 extends Solution {
   }
 
   @Override
-  protected Object part1() {
-    return null;
+  protected Long part1() {
+    return calculate(80);
   }
 
   @Override
-  protected Object part2() {
-    return null;
+  protected Long part2() {
+    return calculate(256);
+  }
+
+  private long calculate(int days) {
+    var fish = new long[9];
+    for (int n : parseInts(first(input()))) {
+      fish[n]++;
+    }
+
+    for (var i = 0; i < days; i++) {
+      var shifted = new long[9];
+      arraycopy(fish, 1, shifted, 0, 8);
+      shifted[6] += fish[0];
+      shifted[8] = fish[0];
+      fish = shifted;
+    }
+    return LongStream.of(fish).sum();
   }
 }
