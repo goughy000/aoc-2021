2c5ff079f60534f57f4384ed0ce28c4730daf7c8
Jack Gough <goughy000@gmail.com>
2021-12-12 15:13:35 +0000

    Simplify

--- day12/src/main/java/com/github/goughy000/Day12.java
+++ day12/src/main/java/com/github/goughy000/Day12.java
@@ -1,10 +1,10 @@
 package com.github.goughy000;
 
 import static com.github.goughy000.Collections2.*;
-import static java.util.Collections.emptyList;
-import static java.util.Collections.singletonList;
+import static java.util.Collections.*;
+import static java.util.function.Function.*;
 import static java.util.function.Predicate.not;
-import static java.util.stream.Collectors.toMap;
+import static java.util.stream.Collectors.*;
 
 import java.util.*;
 import java.util.function.BiPredicate;
@@ -20,25 +20,20 @@ public class Day12 extends Solution {
 
   @Override
   protected Long part1() {
-    return routes(links(), (path, next) -> isBig(next) || !path.contains(next)).count();
+    return routes((path, next) -> isBig(next) || !path.contains(next)).count();
   }
 
   @Override
   protected Long part2() {
-    var links = links();
-
-    return links.keySet().stream()
-        .filter(not(this::isTerminal))
-        .filter(not(this::isBig))
-        .flatMap(
-            cave ->
-                routes(
-                    links,
-                    (path, next) ->
-                        isBig(next)
-                            || !path.contains(next)
-                            || (cave.equals(next) && count(path, next) == 1L)))
-        .distinct()
+    return routes(
+            (path, next) ->
+                isBig(next)
+                    || (!START.equals(next)
+                        && (!path.contains(next)
+                            || !path.stream()
+                                .filter(not(this::isBig))
+                                .collect(groupingBy(identity(), counting()))
+                                .containsValue(2L))))
         .count();
   }
 
@@ -46,40 +41,32 @@ public class Day12 extends Solution {
     return name.toUpperCase().equals(name);
   }
 
-  private boolean isTerminal(String name) {
-    return START.equals(name) || END.equals(name);
-  }
-
-  private static Stream<List<String>> routes(
-      Map<String, List<String>> links, BiPredicate<List<String>, String> traverse) {
-    return routes(START, END, links, emptyList(), traverse);
+  private Stream<List<String>> routes(BiPredicate<List<String>, String> traverse) {
+    var links =
+        input().stream()
+            .map(Collections2::parseList)
+            .flatMap(parts -> Stream.of(parts, List.of(last(parts), first(parts))))
+            .collect(
+                toMap(
+                    Collections2::first,
+                    parts -> singletonList(last(parts)),
+                    Collections2::concat));
+    return routes(START, links, emptyList(), traverse);
   }
 
   private static Stream<List<String>> routes(
       String start,
-      String end,
       Map<String, List<String>> links,
       List<String> current,
       BiPredicate<List<String>, String> traverse) {
     var path = concat(current, start);
-
     return links.get(start).stream()
         .flatMap(
-            next -> {
-              if (end.equals(next)) {
-                return Stream.of(concat(path, next));
-              } else if (traverse.test(current, next)) {
-                return routes(next, end, links, path, traverse);
-              }
-              return Stream.empty();
-            });
-  }
-
-  private Map<String, List<String>> links() {
-    return input().stream()
-        .map(Collections2::parseList)
-        .flatMap(parts -> Stream.of(parts, List.of(last(parts), first(parts))))
-        .collect(
-            toMap(Collections2::first, parts -> singletonList(last(parts)), Collections2::concat));
+            next ->
+                END.equals(next)
+                    ? Stream.of(concat(path, next))
+                    : traverse.test(path, next)
+                        ? routes(next, links, path, traverse)
+                        : Stream.empty());
   }
 }
